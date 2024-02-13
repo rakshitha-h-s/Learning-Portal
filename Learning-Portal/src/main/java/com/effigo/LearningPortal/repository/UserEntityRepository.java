6cb7f4fad247c8b700d153e7aa933be17bea571b
RAKSHITHA H S <95288542+rakshitha-h-s@users.noreply.github.com>
2024-02-13 17:37:12 +0530

    Delete Learning-Portal directory

--- Learning-Portal/src/main/java/com/effigo/LearningPortal/repository/UserEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.effigo.LearningPortal.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.effigo.LearningPortal.entity.UserEntity;
-@Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity,Long>{
-
-}
